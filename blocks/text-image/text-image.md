ccdc24730808dbbe0aee0485f6e18eb259c94994
uajw <uajw@novonordisk.com>
2026-03-13 10:20:27 +0530

    fixing git issues

--- blocks/text-image/text-image.md
+++
@@ -1,5 +0,0 @@
-## Text with Image
-| Image             | Text                          |
-|-------------------|-------------------------------|
-| /images/sample.jpg| Welcome to Adobe EDS!         |
-| /images/banner.png| Fast delivery at the edge.    |
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -217,10 +217,4 @@ async function loadPage() {
   await loadLazy(document);
   loadDelayed();
 }
-(async function loadDa() {
-  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
-  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
-}());
-
-
 loadPage();
